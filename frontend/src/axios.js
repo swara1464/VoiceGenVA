58ed8930f1c86d0356444d43a4236ddb594cf1d0
swara1464 <153909554+swara1464@users.noreply.github.com>
2025-12-06 12:28:29 +0530

    Update axios.js
    added JWT, withCredentials=false;

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = false;
 axios.defaults.baseURL = "https://vocalagentapi.onrender.com";
 
 export default axios;
